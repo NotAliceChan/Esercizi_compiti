23e71788fbbc8d2282508abe4b41040a5af6586c
Alice <fenuwu99@gmail.com>
2023-06-10 23:04:08 +0200

    fix esercizio numero 9

--- esercizio_9/switc.java
+++ esercizio_9/switc.java
@@ -1,36 +1,55 @@
 package esercizio_9;
 
+import java.util.Scanner;
+
 public class switc {
     public static void main(String[] args) {
-        int number = 11;
+
+        int number;
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Please Enter the Number");
+            number = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (number) {
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+                case 10:
+                    System.out.println("Tem");
+                    break;
+                default:
+                    System.out.println("Error Cannot give you the name");
+                    break;
 
 
-        if (number == -1) {
-
-        } else if (number == 1) {
-            System.out.println("1 > ONE");
-        } else if (number == 2 ){
-            System.out.println("2 > TWO");
-        } else if (number == 3) {
-            System.out.println("3 > THREE");
-        } else if (number == 4) {
-            System.out.println("4 > FOUR");
-        } else if (number == 5) {
-            System.out.println("5 > FIVE");
-        } else if (number == 6) {
-            System.out.println("6 > SIX");
-        } else if (number == 7) {
-            System.out.println("7 > SEVEN");
-        } else if (number == 8) {
-            System.out.println("8 > EIGHT");
-        } else if (number == 9) {
-            System.out.println("9 > NINE");
-        } else if (number == 10) {
-            System.out.println("10 > TEN");
-        } else {
-            System.out.println("Error Cannot give you the name");
+            }
         }
-
     }
-
 }
